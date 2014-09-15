c975cc99b31416cb3e12694c68f0dbbd0bc4f714
Owner <Owner@VAIO.wi.rr.com>
2014-09-15 14:56:35 -0500

    Fixed Abstract Class

--- src/myabstract/DemoMyAbstract.java
+++ src/myabstract/DemoMyAbstract.java
@@ -12,11 +12,23 @@ package myabstract;
  */
 public class DemoMyAbstract {
 
+    String message1;
+    String message2;
+    String message3;
+    
     public static void main(String[] args) {
+        DemoMyAbstract d = new DemoMyAbstract();
+        
         myabstract.Employee employee = new myabstract.SalariedEmployee(60000,"Jones", "Tom", "AAAA", "Sales manager");
         myabstract.Employee hourlyEmployee = new myabstract.HourlyEmployee(35.50, "Sommers", "Bob","E1211", "Engineer");
         myabstract.Employee salaryPlusCommissionEmployee = new myabstract.SalaryPlusCommissionEmployee(25000, 100000, "Edison", "Thomas", "BBBB", "Sales Rep");
         
+        d.message1 = ("Alias: " + employee.getAliasID()
+                     + "\nName: " + employee.getLastName() + ", " + employee.getFirstName()
+                     + "\nJob Title: " + employee.getJobTitle()
+                     + "\nHourly Wage: ");          
+
+        System.out.println("");
         
     }
 }
